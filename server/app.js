3bb939685a258fe53189a3ea4e3c773dae862c4f
kidow <wcgo2ling@hanmail.net>
2019-01-09 14:44:41 +0900

    basic setting

--- server/app.js
+++ server/app.js
@@ -1,1 +1,14 @@
 const express = require('express')
+const morgan = require('morgan')
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.json())
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/', require('./routes'))
+
+app.listen(4000, () => {
+  console.log('App listening on port 4000!')
+})
